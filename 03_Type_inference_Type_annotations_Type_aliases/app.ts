977eb808d734b698d20e04e59da39668024bbb6c
RRayied991 <hussainrayied9@gmail.com>
2025-08-18 03:32:41 +0600

    Readme file updated

---
+++ 03_Type_inference_Type_annotations_Type_aliases/app.ts
@@ -0,0 +1,20 @@
+//Type Inference: didn't tell the variable the type, it initializes automatically
+
+let a = "12";
+
+//Type Annotations
+
+let c: Number | Boolean | String;
+c = 12;
+c = "13";
+c = "false";
+
+let b: Boolean;
+
+function abcd(a: Number, b: String): void {}
+
+//Defining Interfaces
+//Using interfaces to define object shapes
+// extending interfaces
+//Type aliases
+//Intersection Types
