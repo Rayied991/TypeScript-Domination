e5f230c6d86b16934a6b98bdd0ac01bad4bb8ae5
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-01-30 12:42:41 +0600

    Modules import and export

---
+++ 08-Modules/app.ts
@@ -0,0 +1,13 @@
+// Modules
+// Exporting and importing modules
+// Default exports
+
+
+// import {addPayment,getdetails} from "./payment";
+
+// addPayment(12);
+
+
+// import payment from "./payment";
+// let a =new payment(12);
+// console.log(a);
---
+++ 08-Modules/payment.ts
@@ -0,0 +1,6 @@
+// export function addPayment(val:number){console.log(val)}
+// export function getdetails(){}
+
+export default class payment{
+    constructor(public price:number){}
+}
